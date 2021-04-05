9a05f1f43d3a44d37c892cdb7a071151bdb96320
samrat1223 <samratganguly37@gmail.com>
2021-04-05 15:54:32 +0530

    User authentication added ,  LoginForm component added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import {ChatEngine} from 'react-chat-engine'
+import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = 'ffac8f54-c51f-4f1d-8da2-5f055772810e';
+
 const App = () => {
-    return(
-        <ChatEngine 
-         height="100vh" //entire project screen
-         projectID="ffac8f54-c51f-4f1d-8da2-5f055772810e"
-         userName="Asamate"
-         userSecret="Asamate"
-         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-        />
-    );
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,35 +1,30 @@
-const  TheirMessage = ({lastMessage,message}) => {
-    //Checking if this is the first message by the user
-    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username != message.sender.username; 
-    return(
-        <div className="message-row">
-           {isFirstMessageByUser && (
-               <div 
-                 className="message-avatar"
-                 style={{backgroundImage:`url(${message?.sender?.avatar})`}}
-               />
-           )}
-            {/**
-             * Checking if the message is not firstMessage by the user and handling
-             */}
-
-             {message?.attachments?.length>0 
-                    ?(
-                        <img 
-                          src={message.attachments[0].file}
-                          alt="message-attachment"
-                          className="message-image"
-                          style={{marginLetf:isFirstMessageByUser ? '4px' : '48px'}}
-                        />
-                    ) : (
-                        <div className="message" style={{float:'left',backgroundColor:'#CABCDC',marginLetf:isFirstMessageByUser ? '4px' : '48px'}}>
-                            {message.text}
-                        </div>
-                    )
-               }
-            
-        </div>
-    )
-}
-
-export default TheirMessage;
+const TheirMessage = ({ lastMessage, message }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+  
+    return (
+      <div className="message-row">
+        {isFirstMessageByUser && (
+          <div
+            className="message-avatar"
+            style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
+          />
+        )}
+        {message.attachments && message.attachments.length > 0
+          ? (
+            <img
+              src={message.attachments[0].file}
+              alt="message-attachment"
+              className="message-image"
+              style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+            />
+          )
+          : (
+            <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+              {message.text}
+            </div>
+          )}
+      </div>
+    );
+  };
+  
+  export default TheirMessage;
